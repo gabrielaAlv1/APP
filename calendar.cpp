acdaaf5a51fc3e09192bfab51b60546ae3027cb7
gabrielaAlv1 <69876801+gabrielaAlv1@users.noreply.github.com>
2021-10-28 14:58:28 -0400

    Update calendar.cpp

--- calendar.cpp
+++ calendar.cpp
@@ -5,7 +5,16 @@
 
 using namespace std;
 
-class Date : public Calendar  {
+//class to print the calendar
+void printCalendar(int year)
+	{
+	int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+	int mDays[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	string monthList[12] = { "January", "February", "March", "April", "May", "June",
+						"July", "August", "September", "October", "November", "December"
+	};
+
+class Date : public Calendar {
 public:
 	void setCalendar()
 	{
@@ -20,9 +29,7 @@ int main()
 	Calendar c;
 
 	c.setCalendar;
-	
-	return 0;
-}
-	
 
+	return 0;
+};
 
